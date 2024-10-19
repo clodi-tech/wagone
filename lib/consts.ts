e0428d648813457a0e1b6722301f7ba1fcce9aea
clodi <ciao@clodi.tech>
2024-10-19 23:21:27 +0200

    links

--- lib/consts.ts
+++ lib/consts.ts
@@ -1,4 +1,5 @@
-export const urlInstagram = "https://www.instagram.com/";
-export const urlFacebook = "https://www.facebook.com/";
-export const urlLinkedin = "https://www.linkedin.com/";
+export const urlInstagram = "https://www.instagram.com/wagone_eu/?hl=en";
+export const urlFacebook =
+  "https://www.facebook.com/profile.php?id=61566327838743";
+export const urlLinkedin = "https://www.linkedin.com/company/wagone";
 export const urlEmail = "mailto:contact.wagone@gmail.com";
