dbf0f5d591f27820bf0cbce9ac20ada3764b9f85
clodi <ciao@clodi.tech>
2024-10-25 16:02:59 +0200

    spacer div and setup pages

--- components/root-header.tsx
+++ components/root-header.tsx
@@ -29,46 +29,49 @@ export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="fixed top-0 left-0 w-full flex items-center justify-left p-4 bg-primary z-50">
-      {" "}
-      {/* Added fixed, top-0, left-0, w-full, and z-50 */}
-      <Logo />
-      <nav className="hidden md:flex space-x-6 text-accent-foreground ml-6">
-        <Link href="/#itineraries">Itineraries</Link>
-        <Link href="/blog">Blog</Link>
-        <Link href="/about">About</Link>
-      </nav>
-      <div className="hidden md:block ml-auto">
+    <>
+      <header className="fixed top-0 left-0 w-full flex items-center justify-left p-4 bg-primary z-50">
         {" "}
-        {/* Added ml-auto to push it to the right */}
-        <Link href="#newsletter-form">
-          <Button className="bg-muted hover:bg-accent hover:text-white text-black mr-8">
-            <Mail className="mr-2 h-4 w-4" />
-            Newsletter
-          </Button>
-        </Link>
-      </div>
-      <Sheet open={isOpen} onOpenChange={setIsOpen}>
-        <SheetTrigger asChild>
-          <Button variant="ghost" size="icon" className="md:hidden">
-            <Menu className="h-6 w-6" />
-            <span className="sr-only">Toggle menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="right" className="w-[300px]">
-          <nav className="flex flex-col space-y-4 mt-8">
-            <Link href="/itineraries">Itineraries</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href="/about">About</Link>
-            <Link href="#newsletter-form">
-              <Button className="hover:bg-secondary text-white">
-                <Mail className="mr-2 h-4 w-4" />
-                Newsletter
-              </Button>
-            </Link>
-          </nav>
-        </SheetContent>
-      </Sheet>
-    </header>
+        {/* Added fixed, top-0, left-0, w-full, and z-50 */}
+        <Logo />
+        <nav className="hidden md:flex space-x-6 text-accent-foreground ml-6">
+          <Link href="/#itineraries">Itineraries</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/about">About</Link>
+        </nav>
+        <div className="hidden md:block ml-auto">
+          {" "}
+          {/* Added ml-auto to push it to the right */}
+          <Link href="#newsletter-form">
+            <Button className="bg-muted hover:bg-accent hover:text-white text-black mr-8">
+              <Mail className="mr-2 h-4 w-4" />
+              Newsletter
+            </Button>
+          </Link>
+        </div>
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger asChild>
+            <Button variant="ghost" size="icon" className="md:hidden">
+              <Menu className="h-6 w-6" />
+              <span className="sr-only">Toggle menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="right" className="w-[300px]">
+            <nav className="flex flex-col space-y-4 mt-8">
+              <Link href="/itineraries">Itineraries</Link>
+              <Link href="/blog">Blog</Link>
+              <Link href="/about">About</Link>
+              <Link href="#newsletter-form">
+                <Button className="hover:bg-secondary text-white">
+                  <Mail className="mr-2 h-4 w-4" />
+                  Newsletter
+                </Button>
+              </Link>
+            </nav>
+          </SheetContent>
+        </Sheet>
+      </header>
+      <div className="h-24" /> {/* Spacer div */}
+    </>
   );
 }
